c11af8efd7af87ee4e0460440507e009ee305231
matt manes <mattmanes@pm.me>
2023-06-10 15:31:08 -0500

    Initial commit

---
+++ src/packagelister/pipmappings.py
@@ -0,0 +1,4 @@
+""" These are packages whose name doesn't match their `pip install` name.
+When `packagelister` is generating a requirements file, these substitutions will be made."""
+
+pipmappings = {"speech_recognition": "SpeechRecognition"}
